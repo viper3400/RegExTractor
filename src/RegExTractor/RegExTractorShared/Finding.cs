0775f04b58a276ba44348b25d7585a12707202eb
Jan Graefe <jan.graefe@gmx.de>
2015-08-07 18:59:59 +0200

    inital commit, created key structure, implmented IRegExCrawler interface and core models

---
+++ src/RegExTractor/RegExTractorShared/Finding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegExTractorShared
+{
+    public class Finding
+    {
+        string Expression { get; set; }
+        string ExpressionFriendlyName { get; set; }
+        string FileName { get; set; }
+        string FileFolder { get; set; }
+        Dictionary<int, Dictionary<int, string>> Match;
+    }
+}
---
+++ src/RegExTractor/RegExTractorShared/RegExSearchTerm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegExTractorShared
+{
+    public class RegExSearchTerm
+    {
+        string Expression { get; set; }
+        string ExpressionFriendlyName { get; set; }
+    }
+}
